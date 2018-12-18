b864ff993c527babaa54ba06f088c40d3c76aa1b
Overzet <overzet@gmail.com>
2018-12-18 13:33:23 +0100

    Initialization

---
+++ src/main/java/be/intecbrussel/mountainserver/MountainServerApplication.java
@@ -0,0 +1,14 @@
+package be.intecbrussel.mountainserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountainServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MountainServerApplication.class, args);
+    }
+
+}
+
